f75f3efef45288409b064a0ccd1a80b56095860a
abdullahalkheshen <abdullah.alkheshen@gmail.com>
2023-12-19 00:34:06 +0200

    Create Die component

--- App.js
+++ App.js
@@ -1,9 +1,20 @@
 import React from "react"
+import Die from "./Die"
 
 export default function App( ) {
+    
     return (
-        <main>
-            <h1> App here </h1>
+        <main className="dice-container">
+            <Die value="1" />
+            <Die value="2" />
+            <Die value="3" />
+            <Die value="4" />
+            <Die value="5" />
+            <Die value="6" />
+            <Die value="1" />
+            <Die value="1" />
+            <Die value="1" />
+            <Die value="1" />
         </main>
     )
 }
---
+++ Die.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Die (props) {
+    return (
+        <div className="die-face">
+            <h1 className="die-num" >{props.value}</h1>
+        </div>
+    )
+} 
+
