0975299cc6803eb69f74a5f64dc1bf0393be9754
abdullahalkheshen <abdullah.alkheshen@gmail.com>
2023-12-19 00:57:38 +0200

    Map each random nb in array to it's Die component

--- App.js
+++ App.js
@@ -1,8 +1,10 @@
-import React from "react"
+import React, { useState } from "react"
 import Die from "./Die"
 
 export default function App( ) {
     
+    const [dice, setDice] = React.useState(allNewDice())
+
     function allNewDice() {
         const newDice = []
         let randomNumber = Math.floor(Math.random() * 6)
@@ -12,18 +14,13 @@ export default function App( ) {
         return newDice
     }
 
+    diceElements = dice.map(
+        die => <Die value={die} />
+    )
+
     return (
         <main className="dice-container">
-            <Die value="1" />
-            <Die value="2" />
-            <Die value="3" />
-            <Die value="4" />
-            <Die value="5" />
-            <Die value="6" />
-            <Die value="1" />
-            <Die value="1" />
-            <Die value="1" />
-            <Die value="1" />
+            {diceElements}
         </main>
     )
 }
